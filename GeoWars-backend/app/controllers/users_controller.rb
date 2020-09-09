8e69e6b42e6e4ae53968b4bab664e7aae15259f9
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-09-09 18:31:32 -0500

    fixed username verify fetch

--- GeoWars-backend/app/controllers/users_controller.rb
+++ GeoWars-backend/app/controllers/users_controller.rb
@@ -30,9 +30,9 @@ class UsersController < ApplicationController
         @user = User.find_by(username: user_params[:username])
         
         if @user && @user.authenticate(user_params[:password])
-            render json: @user.to_json
-            #     only: [:id, :username]
-            # )
+            render json: @user.to_json(
+                only: [:id, :username]
+            )
         else
             render :json => {"message": "This username & password combination is invalid. Create an account or try again."}
         end
