f5a47677ecf6159eda6c271e2fb5234bcf2b8b7b
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-09-10 17:04:51 -0500

    some styling

--- GeoWars-backend/app/models/user.rb
+++ GeoWars-backend/app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
     has_secure_password
     
+    validates :username, presence: true
     validates :username, uniqueness: true, on: :create
     validates :password, length: {in:6..20}
     
