cc87e121ee564332c40d0707d852ea292b3a11d2
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-09-11 10:08:21 -0500

    more styling and better seed file

--- GeoWars-backend/db/seeds.rb
+++ GeoWars-backend/db/seeds.rb
@@ -9,6 +9,16 @@
 
 dan = User.create(username: "daniel", password: "daniel", password_confirmation: "daniel")
 yehong = User.create(username: "yehong", password: "yehong", password_confirmation: "yehong")
+jaz = User.create(username: "jazzy", password: "kittycat", password_confirmation: "kittycat")
+mello = User.create(username: "mello", password: "puppydog", password_confirmation: "puppydog")
 
-score1 = Score.create(score:1000000, time_alive: 1000, accuracy: 100, user_id: yehong.id)
-score1 = Score.create(score:500000, time_alive: 500, accuracy: 50, user_id: dan.id)
+score1 = Score.create(score:99, time_alive: 65.205, accuracy: 5.50, user_id: yehong.id)
+score2 = Score.create(score:75, time_alive: 52.107, accuracy: 9.20, user_id: dan.id)
+score3 = Score.create(score:65, time_alive: 40.300, accuracy: 9.25, user_id: jaz.id)
+score4 = Score.create(score:90, time_alive: 65.705, accuracy: 6.20, user_id: mello.id)
+score5 = Score.create(score:135, time_alive: 80.500, accuracy: 4.50, user_id: yehong.id)
+score6 = Score.create(score:115, time_alive: 70.659, accuracy: 7.10, user_id: dan.id)
+score7 = Score.create(score:30, time_alive: 20.010, accuracy: 15.25, user_id: jaz.id)
+score8 = Score.create(score:80, time_alive: 59.475, accuracy: 7.20, user_id: mello.id)
+score9 = Score.create(score:35, time_alive: 23.300, accuracy: 11.25, user_id: jaz.id)
+score10 = Score.create(score:5, time_alive: 5.100, accuracy: 50.50, user_id: dan.id)
